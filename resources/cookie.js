1825ed0acc0337bdeddbf0fcde673632da1a5cc7
Gustavo Babboni Carli <141583900+oaocd1@users.noreply.github.com>
2025-05-22 12:59:18 -0700

    cc
    cc

--- resources/cookie.js
+++ resources/cookie.js
@@ -1,13 +1,14 @@
 function getCookie(cname) {
     let name = cname + "=";
+
+    // Pega todos os cookies do documento e decodifica para tratar caracteres especiais
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
-    
     for (let i = 0; i < ca.length; i++) {
-        let c = ca[i].trim(); // Usando trim() para remover espaços extras
-        
+        let c = ca[i].trim();
+
         if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length); // Retorna o cookkie
+            return c.substring(name.length, c.length);
         }
     }
     
--- resources/user.js
+++ resources/user.js
@@ -8,7 +8,7 @@ async function loadUserInfo() {
         const response = await fetch('http://localhost:8080/user', {
             method: 'GET',
             headers: {
-                'Authorization': `${token}`, // Passando o token para a requisição
+                'Authorization': `${token}`, 
             }
         });
 
@@ -16,16 +16,16 @@ async function loadUserInfo() {
             throw new Error(`Erro ao carregar dados do usuário: ${response.statusText}`);
         }
 
-        const responseText = await response.text();  // Pega a resposta como texto
+        const responseText = await response.text();  
 
-        console.log("Resposta do servidor:", responseText);  // Exibe a resposta no console
+        console.log("Resposta do servidor:", responseText); 
 
-        const userData = JSON.parse(responseText);  // Tenta converter a resposta em JSON
+        const userData = JSON.parse(responseText); 
 
         if (userData.data && userData.data.length > 0) {
-            // Aqui pegamos o primeiro usuário ou você pode ajustar para pegar o usuário desejado com base em outro critério
-            const user = userData.data[0];  // Pega o primeiro usuário da lista (ou altere para outro critério, como ID)
-            document.getElementById('user-name').textContent = user.name;  // Exibe o nome do usuário
+         
+            const user = userData.data[0];  
+            document.getElementById('user-name').textContent = user.name;  
         } else {
             console.error('Erro: Nenhum usuário encontrado');
             document.getElementById('user-name').textContent = 'Nome não encontrado';
@@ -38,7 +38,7 @@ async function loadUserInfo() {
 }
 
 
-// Chama a função para carregar o nome do usuário ao carregar a página
+
 document.addEventListener("DOMContentLoaded", function() {
     loadUserInfo();
 });
